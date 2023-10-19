7b8c72b2f4c3ac3240a6e45c5bdac2829c1503fb
Katherine Vail <jabberduckygame@gmail.com>
2023-10-19 16:59:20 -0500

    Adding functions

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const apiKey = API_KEY;
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=huntsville";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = Document.querySelector(".search input");
+const searchBtn = Document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = Math.round (data.wind.speed) + "mph";
 }
 
-
-checkWeather();
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
